d411491bcb5e39e53c3e07fd5a558d8ae82fb41d
skpremkumar2005 <skpremkumarsellamuthu@gmail.com>
2025-04-18 15:07:06 +0530

    development change

--- Backend/utils/token.js
+++ Backend/utils/token.js
@@ -3,11 +3,12 @@ import jwt from 'jsonwebtoken';
 const generatetokens=(userId,res)=>{
     const token =jwt.sign({userId},process.env.jwt,{expiresIn:'10d'})
     
-    res.cookie("jwt",token,{
-        maxAge:10*24*60*1000,
-        httpOnly:true,//xss 
-        sameSite:'strict',//csrf
-        secure:process.env.NODE_ENV!=='developmen',
-    })
+    res.cookie("jwt", token, {
+        maxAge: 10 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+    });
+    
 }
 export default generatetokens;
